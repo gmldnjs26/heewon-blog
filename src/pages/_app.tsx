882493d99f4f3b2fc906f71d6e2d3e33343c3eb2
gmldnjs26 <imleesky265@gmail.com>
2022-11-11 00:51:04 +0900

    fix: SSR을 위한 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,43 @@
+import styled from 'styled-components';
 import { AppProps as NextAppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../utils/theme';
+import createEmotionCache from '../utils/createEmotionCache';
+
+const clientSideEmotionCache = createEmotionCache();
+
 type AppProps<P = any> = {
   pageProps: P;
+  emotionCache?: EmotionCache;
 } & Omit<NextAppProps<P>, 'pageProps'>;
 
-import 'src/styles/globals.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import 'src/styles/globals.scss';
+import Navigation from '../components/Navigation';
 
-const App = ({ Component, pageProps }: AppProps<any>) => {
-  return <Component {...pageProps} />;
+const StApp = styled.div`
+  width: 1000px;
+  margin: auto;
+`;
+
+const App = ({ Component, pageProps, emotionCache = clientSideEmotionCache }: AppProps<any>) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <StApp>
+          <Navigation styles={{ mt: '16px' }} />
+          <Component {...pageProps} />
+        </StApp>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 };
 
 export default App;
