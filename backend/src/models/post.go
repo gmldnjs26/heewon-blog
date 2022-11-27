44b38b51fc8e00754ad60dbbe1ef97e82a31057c
gmldnjs26 <imleesky265@gmail.com>
2022-11-27 17:29:20 +0900

    feat(back): add post model

---
+++ backend/src/models/post.go
@@ -0,0 +1,11 @@
+package models
+
+type Post struct {
+	Model
+	CategoryId      int16  `json:"category_id"`
+	Title           string `json:"title"`
+	Contents        string `json:"contents"`
+	PreviewContents string `json:"preview_contents"`
+	Password        string `json:"password"`
+	Status          int    `json:"status"`
+}
