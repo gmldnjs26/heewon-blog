99424f73fb6c6dfd4bc0208be2a36b12dd38dd05
gmldnjs26 <imleesky265@gmail.com>
2022-12-03 15:09:39 +0900

    config: set db

--- backend/src/database/mysql.go
+++ backend/src/database/mysql.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -9,11 +11,14 @@ var DB *gorm.DB
 
 func Connect() {
 	var err error
-	dsn := "root:root@tcp(db:3306)/ambassador"
+	dsn := "root:root@tcp(db:3306)/hwblog"
 
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
+		fmt.Println(err)
 		panic("DB Connection Error")
+	} else {
+		panic("DB Connection Success")
 	}
 }
