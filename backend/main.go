9c7464f9d75df9ddca0b024df4b89858f7a274f0
gmldnjs26 <imleesky265@gmail.com>
2022-12-08 23:29:47 +0900

    feat(back): set cors

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"heewon-blog/src/controllers"
 	"heewon-blog/src/database"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -13,9 +15,21 @@ func main() {
 
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000",
+		AllowCredentials: true,
+		// TODO: 좀더 조사
+		// cookie의 정보를 허용된 origin에서는 취득할 수 있게끔 설정? 응답헤더에 담아서 보내는건데 프론트에서 이 쿠키를 취득할 수 있게끔 한다?
+		// 근데 httpOnly 쿠키인데도 프론트에서 취득할 수 있나? 취득이라기 보다는 request 헤더에 담을 수 있게끔 하는 설정인가?
+		// httpOnly는 javascript로 쿠키로 접근 못하게끔 하는 설정이다. 프론트에서 쿠키를 취득할 액션을 취할 필요는 없고 자동으로 취득하게 된다.
+		// request를 보낼때 서버에서 쿠키를 받아 브라우저에서 저장중이라면 자동으로 header에 실려서 날라간다.
+	}))
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("This is heewon blog")
 	})
+
+	app.Get("/posts", controllers.Posts)
 
 	app.Listen(":5000")
 }
